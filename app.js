2a9d5a9cf5d04e2d9bf29daa14fb574f70e5675d
Marjan Ghazanfari <ghazanfari.marjan@gmail.com>
2021-04-19 09:03:59 +0200

    with path and express.static

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
+app.use(express.static('website'));
+
 app.get('/', (req, res) => {
-    res.send('nothing')
+    res.sendFile(path.join(__dirname, 'website', 'index.html'));
 });
 
 module.exports = app;
